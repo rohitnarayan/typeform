2438a7d5ae0b1a2d604b0072f9f0abfdce459189
Rohit Jaiswal <rohit.jaiswal@connvertex.com>
2017-07-26 18:03:22 +0530

    Visible viewport keyboard event scroll

--- appController.js
+++ appController.js
@@ -6,7 +6,7 @@
 
 			var vm = this;
 
-			vm.showButtonThresholdValue=0;
+			// $window.showButtonThresholdValue=0;
 
 			vm.options = [
 				checkboxDataService.option1,
@@ -22,22 +22,63 @@
 				$window.onkeydown = function(e) {
 					$window.code = e.keyCode ? e.keyCode:e.which;
 					// console.log(vm.$elements);
-					angular.forEach(vm.$elements,function(value,i) {
-						// console.log(value.children[1].innerHTML);
-						// console.log($window.code);
-						// vm.input = value.children;
-						if(value.children[1].innerHTML.charCodeAt(0) == $window.code) {
-							// console.log("HAHA = "+value.children[1].innerHTML);
-							// value.children[0].checked = true;
-							if(value.children[0].checked) {
-								value.children[0].checked=false;
+					if($window.code != 13) {
+						angular.forEach(vm.$elements,function(value,i) {
+							// console.log(value.children[1].innerHTML);
+							// console.log($window.code);
+							// vm.input = value.children;
+							if(value.children[1].innerHTML.charCodeAt(0) == $window.code) {
+								// console.log("HAHA = "+value.children[1].innerHTML);
+								// value.children[0].checked = true;
+								if(value.children[0].checked) {
+									value.children[0].checked=false;
+									// $window.showButtonThresholdValue-=1;
+								}
+								else {
+									value.children[0].checked=true;
+									// $window.showButtonThresholdValue+=1;
+									// console.log(vm.showButtonThresholdValue);
+								}
+								// vm.counter = $window.showButtonThresholdValue;
 							}
-							else {
-								value.children[0].checked=true;
-							}
-						}
-					});
+						});
+					}
+					else if(code == 13) {
+						var goTo = $('p')[0].offsetTop;
+						$('html, body').animate({
+						    scrollTop: goTo
+						}, 500);
+					}
 				}
 			}
+
+			// $(window).on('keypress', function(event){
+			//     event.preventDefault();
+
+			//     var code = event.keyCode?event.keyCode:event.which;
+			//     // console.log(code);
+			//     // console.log($('p')[0].offsetTop);
+			//     if(code == 13) {
+			//     	var goTo=$('p')[0].offsetTop;
+			//     	// console.log($( $.attr(this, 'href') ).offset().top);
+
+			//     	$('html, body').animate({
+			//     	    scrollTop: goTo
+			//     	}, 500);
+			//     }
+			// });
+			// $window.onkeydown = function(e) {
+			// 	var code = e.keyCode ? e.keyCode:e.which;
+			// 	if(code == 13) {
+			// 		e.preventDefault();
+			// 		var a = $('a');
+			// 		console.log(a[0].offsetTop);
+
+			// 		$('html, body').animate({
+			// 			scrollTop:$()
+			// 		});
+			// 	}
+			// }
+
 		}]);
 })();
